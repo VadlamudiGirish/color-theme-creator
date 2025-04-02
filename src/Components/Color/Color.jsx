202195b529bf0c0d71d43ba2e92d470a90595916
Girish Vadlamudi <girishvadlamudi@outlook.com>
2025-04-02 15:53:23 +0200

    Implement color update by re-using the colorForm component

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,8 +1,19 @@
 import { useState } from "react";
 import "./Color.css";
+import ColorForm from "../ColorForm/ColorForm";
 
-export default function Color({ color, onDelete }) {
+export default function Color({ color, onDelete, onUpdate }) {
   const [confirmMessage, setConfirmMessage] = useState(false);
+  const [editMode, setEditMode] = useState(false);
+
+  function handleColorUpdate(UpdatedColor) {
+    setEditMode(false);
+    onUpdate(UpdatedColor);
+  }
+
+  function handleColorDelete() {
+    onDelete(color.id);
+  }
 
   return (
     <div
@@ -15,27 +26,61 @@ export default function Color({ color, onDelete }) {
       <h3 className="color-card-hightlight">{color.hex}</h3>
       <h4>{color.role}</h4>
       <p>contrast: {color.contrastText}</p>
-      {!confirmMessage ? (
-        <button
-          onClick={() => setConfirmMessage(true)}
-          className="color-card-hightlight"
-        >
-          Delete
-        </button>
-      ) : (
-        <div className="color-card-highlight">
-          <p>Really delete?</p>
-          <button onClick={() => onDelete(color.id)} style={{ margin: "5px" }}>
-            Yes
-          </button>
-          <button
-            onClick={() => setConfirmMessage(false)}
-            style={{ margin: "5px" }}
-          >
-            No
-          </button>
-        </div>
-      )}
+
+      <div className="action-container">
+        {confirmMessage ? (
+          <div className="color-card-highlight">
+            <p>Really delete?</p>
+            <button
+              onClick={handleColorDelete}
+              style={{ backgroundColor: color.contrastText, color: color.hex }}
+            >
+              Yes
+            </button>
+            <button
+              onClick={() => setConfirmMessage(false)}
+              style={{ backgroundColor: color.contrastText, color: color.hex }}
+            >
+              No
+            </button>
+          </div>
+        ) : editMode ? (
+          <ColorForm
+            onSubmitColor={handleColorUpdate}
+            currentColor={color}
+            onCancel={() => setEditMode(false)}
+          />
+        ) : (
+          <div className="button-group">
+            <button
+              onClick={() => {
+                setEditMode(false);
+                setConfirmMessage(true);
+              }}
+              className="color-card-hightlight"
+              style={{
+                border: editMode ? `2px solid ${color.contrastText}` : "none",
+              }}
+            >
+              Delete
+            </button>
+            <button
+              onClick={() => {
+                setConfirmMessage(false);
+                setEditMode(true);
+              }}
+              className="color-card-hightlight"
+              style={{
+                border: confirmMessage
+                  ? `2px solid ${color.contrastText}`
+                  : "none",
+              }}
+            >
+              Edit
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
