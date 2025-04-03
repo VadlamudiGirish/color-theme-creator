babb94eccb29ebf831e716af8be38f1c3cde9132
Girish Vadlamudi <girishvadlamudi@outlook.com>
2025-04-03 12:08:59 +0200

    Implement copy-to-clipboard-button and reset it after 3 seconds

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,9 @@ function App() {
       <h1 className="text">Theme Creator</h1>
       <ColorForm onSubmitColor={handleCreateColor} />
       {colors.length === 0 ? (
-        <p className="text">No colors left! Start adding new colors 🌈</p>
+        <p className="text" style={{ alignSelf: "center" }}>
+          No colors left! Start adding new colors 🌈
+        </p>
       ) : (
         colors.map((color) => {
           return (
